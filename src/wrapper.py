3a9e7690b87e648f32fa925d81288067ca90b69c
Vishal Burman <vishal.a.burman23@gmail.com>
2022-09-10 13:55:16 +0530

    wrapper.py: Create thin wrapper for coref-resolved text

---
+++ src/wrapper.py
@@ -0,0 +1,16 @@
+import spacy
+import neuralcoref
+
+class EasyNeuralCoref:
+    def __init__(self,):
+        self.nlp = spacy.load("en_core_web_sm")
+        neural_coref.add_to_pipe(self.nlp)
+
+    def _coref_text_resolve(text: str) -> str:
+        doc = self.nlp(text)
+        return doc._.coref_resolved
+
+    def __call__(self, text: str) -> str:
+        if not text:
+            raise ValueError(f"Not a valid text")
+        return _coref_text_resolve(text)
