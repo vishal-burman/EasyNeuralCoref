50fbd39232728525dc5f6b9a8b8053f5ff3fa7e9
Vishal Burman <vishal.a.burman23@gmail.com>
2022-09-10 15:37:38 +0530

    wrapper.py: _coref_text_resolve method bug fix

--- easyneuralcoref/wrapper.py
+++ easyneuralcoref/wrapper.py
@@ -6,11 +6,11 @@ class EasyNeuralCoref:
         self.nlp = spacy.load("en_core_web_sm")
         neuralcoref.add_to_pipe(self.nlp)
 
-    def _coref_text_resolve(text: str) -> str:
+    def _coref_text_resolve(self, text: str) -> str:
         doc = self.nlp(text)
         return doc._.coref_resolved
 
     def __call__(self, text: str) -> str:
         if not text:
             raise ValueError(f"Not a valid text")
-        return _coref_text_resolve(text)
+        return self._coref_text_resolve(text)
